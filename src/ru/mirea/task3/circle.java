40f29dd5635c06f9d010903d76fd5b1d739ba6cb
dariux1xx <44808727+dariux1xx@users.noreply.github.com>
2021-12-13 19:26:26 +0300

    Create circle.java

---
+++ src/ru/mirea/task3/circle.java
@@ -0,0 +1,14 @@
+package ru.mirea.task3;
+
+
+public class Circle {
+    private int radius;
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+}
