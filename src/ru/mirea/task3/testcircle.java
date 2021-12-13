b8937b70bf74765c3c94ff64d4859061bb273976
dariux1xx <44808727+dariux1xx@users.noreply.github.com>
2021-12-13 19:26:47 +0300

    Create testcircle.java

---
+++ src/ru/mirea/task3/testcircle.java
@@ -0,0 +1,11 @@
+package ru.mirea.task3;
+
+class CircleTest{
+    public static void main(String[] args) {
+        int rad = 10;
+
+        Circle crc = new Circle();
+        crc.setRadius(rad);
+        System.out.println(crc.getRadius());
+    }
+}
